453ed1ea6ee091b686117e81d8cca473833bb507
root <root@localhost.localdomain>
2025-06-16 15:48:28 +0000

    rotas basicas e arquivos htmls de teste

---
+++ backend/back/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    data_vencimento = models.DateTimeField(auto_now=True)
+    codigo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nome
+
+class Venda(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.produto.nome
+    
